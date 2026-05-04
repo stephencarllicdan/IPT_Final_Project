863d9af77c3e9c6ad122a0eb04000f404edb3c49
angelurapal-hash <angelu.rapal@hcdc.edu.ph>
2026-05-05 02:40:39 +0800

    fix db connection

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
+});
 
 db.connect(err => {
   if (err) {
